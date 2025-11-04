88f6b8049ef1f89a866d0b94597bde5c16f536a0
Gustavo Assalin <gustavo.assalin@hotmail.com>
2025-11-04 16:15:01 -0300

    Adicao config - ms-pessoas

--- backend/ms-pessoas/src/main/java/br/com/unicos/ms_pessoas/MsPessoasApplication.java
+++ backend/ms-pessoas/src/main/java/br/com/unicos/ms_pessoas/MsPessoasApplication.java
@@ -2,8 +2,10 @@ package br.com.unicos.ms_pessoas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsPessoasApplication {
 
 	public static void main(String[] args) {
