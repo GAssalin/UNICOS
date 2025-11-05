50692a79aba2aada723fa58612cdc3f3660751bf
Gustavo Assalin <gustavo.assalin@hotmail.com>
2025-11-05 12:23:21 -0300

    Criacao configs - ms-estoque

--- backend/ms-estoque/src/main/java/br/com/unicos/ms_estoque/MsEstoqueApplication.java
+++ backend/ms-estoque/src/main/java/br/com/unicos/ms_estoque/MsEstoqueApplication.java
@@ -2,8 +2,10 @@ package br.com.unicos.ms_estoque;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsEstoqueApplication {
 
 	public static void main(String[] args) {
