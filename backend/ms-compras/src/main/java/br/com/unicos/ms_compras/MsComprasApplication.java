556b79545762134e3ac0b2424a76aa7020dad61d
Gustavo Assalin <gustavo.assalin@hotmail.com>
2025-11-06 21:21:12 -0300

    Criacao config Eureka - ms-compras

--- backend/ms-compras/src/main/java/br/com/unicos/ms_compras/MsComprasApplication.java
+++ backend/ms-compras/src/main/java/br/com/unicos/ms_compras/MsComprasApplication.java
@@ -1,9 +1,11 @@
 package br.com.unicos.ms_compras;
 
+import lombok.EqualsAndHashCode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EqualsAndHashCode
 public class MsComprasApplication {
 
 	public static void main(String[] args) {
