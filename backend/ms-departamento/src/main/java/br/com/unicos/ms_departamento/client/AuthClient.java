82028d4d4771a7d72523c4a1b9f16b5efbf8dccb
gassalin <gustavo.assalin@hotmail.com>
2026-01-15 16:08:16 -0300

    MS-DEPARTAMENTO - Criação inicial das configs

---
+++ backend/ms-departamento/src/main/java/br/com/unicos/ms_departamento/client/AuthClient.java
@@ -0,0 +1,22 @@
+package br.com.unicos.ms_departamento.client;
+
+import br.com.unicos.core.auth.dto.TokenValidationResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(
+        name = "ms-auth",
+        contextId = "AuthClient"
+)
+public interface AuthClient {
+
+    @PostMapping("/internal/auth/validate-token")
+    TokenValidationResponse validateToken(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorization);
+
+    @PostMapping("/internal/auth/permissions/check")
+    boolean usuarioPossuiPermissao(@RequestParam String nomePermissao);
+
+}
