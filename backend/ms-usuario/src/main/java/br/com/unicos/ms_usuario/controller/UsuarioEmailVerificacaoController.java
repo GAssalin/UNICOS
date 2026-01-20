23bffd1fc337ab764d0405e8ed757a5e0b0c5bcc
gassalin <gustavo.assalin@hotmail.com>
2026-01-20 20:07:28 -0300

    SplitMSAuthPermission - Ajuste imports

--- backend/ms-usuario/src/main/java/br/com/unicos/ms_usuario/controller/UsuarioEmailVerificacaoController.java
+++ backend/ms-usuario/src/main/java/br/com/unicos/ms_usuario/controller/UsuarioEmailVerificacaoController.java
@@ -10,7 +10,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -18,7 +17,6 @@ import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
