869d19c8e6a3a17af941c88368d3e672c5b70da8
gassalin <gustavo.assalin@hotmail.com>
2026-01-19 22:34:14 -0300

    SplitMSAuthPermission - Criacao client de permissao

---
+++ backend/ms-empresa/src/main/java/br/com/unicos/ms_empresa/client/PermissaoClient.java
@@ -0,0 +1,16 @@
+package br.com.unicos.ms_empresa.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(
+        name = "ms-permissao",
+        contextId = "PermissaoClient"
+)
+public interface PermissaoClient {
+
+    @PostMapping("/internal/permissao/check")
+    boolean usuarioPossuiPermissao(@RequestParam String nomePermissao);
+
+}
