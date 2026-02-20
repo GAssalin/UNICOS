44b8d7fc6c4c329b0d2c74cae0047042bcef5509
gassalin <gustavo.assalin@hotmail.com>
2026-02-20 11:42:41 -0300

    MS-Compras - Remocao

--- backend/ms-compras/src/main/java/br/com/unicos/ms_compras/dto/marca/MarcaProdutoResumoResponse.java
+++
@@ -1,7 +0,0 @@
-package br.com.unicos.ms_compras.dto.marca;
-
-public record MarcaProdutoResumoResponse(
-        Long id,
-        String nome,
-        Boolean ativo
-) {}
--- backend/ms-compras/src/main/java/br/com/unicos/ms_compras/dto/produtotipo/ProdutoTipoResumoResponse.java
+++
@@ -1,7 +0,0 @@
-package br.com.unicos.ms_compras.dto.produtotipo;
-
-public record ProdutoTipoResumoResponse(
-        Long id,
-        String nome,
-        Boolean ativo
-) {}
